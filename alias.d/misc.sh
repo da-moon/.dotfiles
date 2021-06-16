36b56047f66597292bc2b6cfca11d36b23d3f523
da-moon <damoon.azarpazhooh@live.com>
2021-06-16 14:27:35 -0400

    nocmt alias
    Signed-off-by: da-moon <contact@havi.dev>

--- alias.d/misc.sh
+++ alias.d/misc.sh
@@ -1,1 +1,2 @@
-  alias dl="aria2c --optimize-concurrent-downloads -k 1M -j16 -x 16 -c --file-allocation=falloc"
+alias dl="aria2c --optimize-concurrent-downloads -k 1M -j16 -x 16 -c --file-allocation=falloc"
+alias nocmt="sed -e '/#/d' -e '/^[[:space:]]*$/d'"
