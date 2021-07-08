77f60643d5b3c393c615f04acb0412e17e99f16f
da-moon <damoon.azarpazhooh@live.com>
2021-07-08 11:36:33 -0400

    pprintenv
    Signed-off-by: da-moon <contact@havi.dev>

--- alias.d/misc.sh
+++ alias.d/misc.sh
@@ -1,2 +1,3 @@
 alias dl="aria2c --optimize-concurrent-downloads -k 1M -j16 -x 16 -c --file-allocation=falloc"
 alias nocmt="sed -e '/#/d' -e '/^[[:space:]]*$/d'"
+alias pprintenv="printenv | awk 'BEGIN{FS=OFS=\"=\";}{printf \"%-30s%-18s\n\",\$1,\$2}'"
