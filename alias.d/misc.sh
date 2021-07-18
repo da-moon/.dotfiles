d644c5d54a5486d2b859004006035c0fb5eb2d23
da-moon <damoon.azarpazhooh@live.com>
2021-07-18 19:19:28 -0400

    code-insiders alias
    Signed-off-by: da-moon <contact@havi.dev>

--- alias.d/misc.sh
+++ alias.d/misc.sh
@@ -1,5 +1,10 @@
 if command -- aria2c -h > /dev/null 2>&1 ; then
   alias dl="aria2c --optimize-concurrent-downloads -k 1M -j16 -x 16 -c --file-allocation=falloc"
 fi
+if ! command -- code -h > /dev/null 2>&1 ; then
+  if command -- code-insiders -h > /dev/null 2>&1 ; then
+    alias code="code-insiders"
+  fi
+fi
 alias nocmt="sed -e '/#/d' -e '/^[[:space:]]*$/d'"
 alias pprintenv="printenv | awk 'BEGIN{FS=OFS=\"=\";}{printf \"%-30s%-18s\n\",\$1,\$2}'"
