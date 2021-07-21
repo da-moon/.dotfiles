a636223d38fd883dd84392445f4ef3777c55580a
da-moon <damoon.azarpazhooh@live.com>
2021-07-21 10:12:51 -0400

    common env
    Signed-off-by: da-moon <contact@havi.dev>

---
+++ env.d/common.sh
@@ -0,0 +1,5 @@
+if [ -d ~/.git-fuzzy ] && command -- fzf -h > /dev/null 2>&1 ; then
+  git -C ~/.git-fuzzy pull > /dev/null 2>&1
+  export PATH="$PATH:$HOME/.git-fuzzy/bin"
+fi
+[ -d /var/lib/snapd/snap/bin ] && export PATH="$PATH:/var/lib/snapd/snap/bin"
