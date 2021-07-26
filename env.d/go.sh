e21e24665eec2ffd661178315094d706744eab83
da-moon <damoon.azarpazhooh@live.com>
2021-07-26 04:00:01 +0000

    fixed go path
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/go.sh
+++ env.d/go.sh
@@ -4,7 +4,7 @@ if [ -d ~/.goenv ]; then
   export PATH="$PATH:$GOENV_ROOT/bin"
   eval "$(goenv init -)"
 fi
-if command -- go -h > /dev/null 2>&1 ; then
+if command -- go version > /dev/null 2>&1 ; then
   export PATH="$PATH:$(go env GOROOT)/bin"
   export PATH="$PATH:$(go env GOPATH)/bin"
   [ "$(go env GO111MODULE)" != "on" ] && go env -w "GO111MODULE=on" 
