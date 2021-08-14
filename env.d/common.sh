7a0bad86328928b3d2267a1bee7a5705d6d65973
da-moon <damoon.azarpazhooh@live.com>
2021-08-13 22:24:40 -0400

    bash history variables
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/common.sh
+++ env.d/common.sh
@@ -5,3 +5,5 @@ fi
 [ -d /var/lib/snapd/snap/bin ] && export PATH="$PATH:/var/lib/snapd/snap/bin"
 [ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin"
 export PATH="${PATH}:${HOME}/.local/bin"
+export HISTFILESIZE=10000
+export PROMPT_COMMAND="history -a && history -c && sort ~/.bash_history | uniq > ~/.bash_history_new && rm ~/.bash_history && mv ~/.bash_history_new ~/.bash_history && history -r"
