9dc676a27b51c496253d73fe5beca0dbb0ddcdf5
da-moon <damoon.azarpazhooh@live.com>
2021-08-21 14:46:33 -0400

    set docker buildkit progress env var to auto
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/docker.sh
+++ env.d/docker.sh
@@ -1,6 +1,6 @@
 if command -- docker -h > /dev/null 2>&1 ; then
   export DOCKER_BUILDKIT=1
-  export BUILDKIT_PROGRESS="plain"
+  export BUILDKIT_PROGRESS="auto"
 fi
 if command -- docker-compose -h > /dev/null 2>&1 ; then
   export COMPOSE_DOCKER_CLI_BUILD=1
