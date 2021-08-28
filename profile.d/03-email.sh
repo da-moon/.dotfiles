7766364e9064eeacff964577bb0847a21b7d5cad
da-moon <damoon.azarpazhooh@live.com>
2021-08-28 13:18:41 -0400

    removed 03-email.sh
    Signed-off-by: da-moon <contact@havi.dev>

--- profile.d/03-email.sh
+++
@@ -1,2 +0,0 @@
-# vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
-himalaya --output json -m Folders/damoon.external search new -s 50000 | jq -r '.response[] | select(.sender | contains("Peak Design")).uid' | xargs -r -I {} himalaya -m Folders/damoon.external delete {}
