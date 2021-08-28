acd49b62a0b76bd7f86f94f3d9100c9f0db6047c
da-moon <damoon.azarpazhooh@live.com>
2021-08-28 11:33:41 -0400

    added devcontainer cli to PATH
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/common.sh
+++ env.d/common.sh
@@ -4,4 +4,10 @@ if [ -d ~/.git-fuzzy ] && command -- fzf -h > /dev/null 2>&1 ; then
 fi
 [ -d /var/lib/snapd/snap/bin ] && export PATH="$PATH:/var/lib/snapd/snap/bin"
 [ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin"
+if command -- pass -h > /dev/null 2>&1 ; then
+  export PASSWORD_STORE_ENABLE_EXTENSIONS=true
+fi
+[ -d "${HOME}/.config/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin" ] \
+  && export PATH="$PATH:${HOME}/.config/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin"
 export PATH="${PATH}:${HOME}/.local/bin"
+
