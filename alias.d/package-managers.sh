f07e11c89a898cb3fe3dd29364095a07c076102c
da-moon <damoon.azarpazhooh@live.com>
2021-09-06 20:28:07 -0400

    aura alias
    Signed-off-by: da-moon <contact@havi.dev>

--- alias.d/package-managers.sh
+++ alias.d/package-managers.sh
@@ -3,5 +3,6 @@ if command -- jq --version > /dev/null 2>&1 \
 && command -- node --version > /dev/null 2>&1 \
 && command -- yarn --version > /dev/null 2>&1 \
 && [ -r $(sudo $(which node) $(which yarn) global dir)/package.json ] ; then
-alias yarn-pkgs='cat  $(sudo $(which node) $(which yarn) global dir)/package.json  | jq -r ".dependencies|keys[]"'
+  alias yarn-pkgs='cat  $(sudo $(which node) $(which yarn) global dir)/package.json  | jq -r ".dependencies|keys[]"'
 fi
+command -- aura --version > /dev/null 2>&1 && alias aura='sudo aura'
