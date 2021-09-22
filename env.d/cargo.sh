393b23f765b79473d70627622a4ef79b8dca3725
da-moon <damoon.azarpazhooh@live.com>
2021-09-22 10:32:53 -0400

    added 'CARGO_HOME' env var
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/cargo.sh
+++ env.d/cargo.sh
@@ -1,2 +1,5 @@
 # vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
-[ -d ~/.cargo/bin ] && export PATH="$PATH:$HOME/.cargo/bin"
+if [ -d "${HOME}/.cargo" ];then
+  export CARGO_HOME="${HOME}/.cargo"
+  export PATH="${CARGO_HOME}/bin:${PATH}"
+fi
