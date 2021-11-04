b4874a732d47e95faebf884399a19f8bd6298345
da-moon <damoon.azarpazhooh@live.com>
2021-11-04 07:35:54 -0400

    feat(alias): added ubuntu container alias
    Signed-off-by: da-moon <contact@havi.dev>

--- alias.d/docker.sh
+++ alias.d/docker.sh
@@ -1,6 +1,8 @@
 # vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
 alias docker-run='docker run --rm -it -v "$PWD:/workspace"'
+alias ubuntu-c='docker-run -w "/workspace" ubuntu:hirsute'
 alias debian-c='docker-run -w "/workspace" debian:bullseye'
 alias alpine-c='docker-run -w "/workspace" alpine:edge'
+
 alias code-server='docker-run --network host -d -u "$(id -u):$(id -g)" codercom/code-server:latest --auth none'
 alias rmcontainers='docker ps -aq | xargs -r docker rm -f'
