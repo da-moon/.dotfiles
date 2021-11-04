ce3eb78a15f1f1f5d53dad3f90cbb43dc4b4f0df
da-moon <damoon.azarpazhooh@live.com>
2021-11-04 18:40:31 -0400

    feat(env): fzf variables
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/fuzzy.sh
+++ env.d/fuzzy.sh
@@ -1,11 +1,8 @@
 # vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
-if command -- sk -h > /dev/null 2>&1 ; then
-export SKIM_DEFAULT_OPTIONS="--preview 'preview {} | head -n 500'"
-export SKIM_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow --no-messages"
-export SKIM_CTRL_T_COMMAND="${SKIM_DEFAULT_COMMAND}"
-# export SKIM_CTRL_T_OPTS=""
-# export SKIM_CTRL_R_OPTS=""
-# Use ~~ as the trigger sequence instead of the default **
-# export SKIM_COMPLETION_TRIGGER='~~'
-export SKIM_COMPLETION_TRIGGER='**'
+
+# https://mike.place/2017/fzf-fd
+if fd -h > /dev/null 2>&1 ; then
+  export FZF_DEFAULT_COMMAND="fd . $HOME"
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="fd -t d . $HOME"
 fi
