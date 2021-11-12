bc7b045dffef66bee4e6e74a317da86a91c8f477
da-moon <damoon.azarpazhooh@live.com>
2021-11-12 14:44:41 -0500

    fix(env): do not set env vars related to Hashicorp product if in Docker container
    Signed-off-by: da-moon <contact@havi.dev>

--- env.d/hashi.sh
+++ env.d/hashi.sh
@@ -1,9 +1,11 @@
 # vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
-if command -- vault -h > /dev/null 2>&1 ; then
-  export VAULT_SKIP_VERIFY="true"
-fi
-if command -- consul -h > /dev/null 2>&1 ; then
-  export CONSUL_SCHEME="https"
-  export CONSUL_HTTP_SSL="true"
-  export CONSUL_HTTP_SSL_VERIFY="false"
+if [ ! -r "/.dockerenv" ]; then
+  if command -- vault -h > /dev/null 2>&1; then
+    export VAULT_SKIP_VERIFY="true"
+  fi
+  if command -- consul -h > /dev/null 2>&1; then
+    export CONSUL_SCHEME="https"
+    export CONSUL_HTTP_SSL="true"
+    export CONSUL_HTTP_SSL_VERIFY="false"
+  fi
 fi
