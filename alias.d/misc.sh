dadef75bc3627e2436607216138ac35fa579fcff
da-moon <contact@havi.dev>
2023-04-28 10:48:30 -0400

    feat(local/bin/update): add topgrade
    Signed-off-by: da-moon <contact@havi.dev>

--- alias.d/misc.sh
+++ alias.d/misc.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
-zoxide --version 
 if [ -x "$(command -v zoxide)" ];then
   alias cd="z" ;
 else
