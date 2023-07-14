a96e0e5aa5281c3fed2b0711c745506ace13e2dd
da-moon <contact@havi.dev>
2023-07-14 05:06:03 -0400

    fix(profile.d/rust): update bash completions for Rust tools
    - Change the check for rustup existence from `$(command -v rustup)` to
    `type -P "rustup" >/dev/null`
    - Add a condition to create a symbolic link if the directory `$(rustc
    --print sysroot)/etc/bash_completion.d` doesn't exist and `/usr/share/
    bash-completion/completions` does exist
    
    Signed-off-by: da-moon <contact@havi.dev>

--- profile.d/rust.sh
+++ profile.d/rust.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # vim: ft=sh syntax=sh softtabstop=2 tabstop=2 shiftwidth=2 fenc=utf-8 expandtab
 
-if [ -x "$(command -v rustup)" ]; then
+if type -P "rustup" >/dev/null; then
+  if { [ ! -d "$(rustc --print sysroot)/etc/bash_completion.d" ] && [ -d "/usr/share/bash-completion/completions" ];  }; then
+    ln -sf "/usr/share/bash-completion/completions" "$(rustc --print sysroot)/etc/bash_completion.d"
+  fi
   eval "$(rustup completions bash rustup)"
   eval "$(rustup completions bash cargo)"
 fi
